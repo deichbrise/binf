735577630452cef954dcbe54e7eda3b4460c574f
Pascal Stammer <stammer@deichbrise.de>
2017-04-05 16:34:31 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ if [ -d bin ]; then
     printf '%s\n' "Removing Lock (bin)"
     rm -rf bin
 fi
+
 mkdir -p bin
 javac -cp ".:lib/*" -d bin $(find ./src/* | grep .java)
 cd bin && jar cvf sources.jar com
