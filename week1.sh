cbff9c2233e3c6e50d61f82ee3c97e75b5197882
Pascal Stammer <stammer@deichbrise.de>
2017-04-05 16:42:08 +0200

    enhance build cycle

--- week1.sh
+++ week1.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+####################
+# Execution Script #
+####################
+
 ./build.sh
+
+echo "======================================================================"
+echo "EXECUTE Tests Week 1"
+echo "======================================================================"
+
 java -cp bin/sources.jar com.week1.test.fibonacci.FibonacciTest
 java -cp bin/sources.jar com.week1.test.fraction.FractionTest
+
+echo "======================================================================"
+echo "SUCCESS"
+echo "======================================================================"
