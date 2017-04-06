0f913e4e75f13b77eb73aff5ca49dbed10624e76
Pascal Stammer <stammer@deichbrise.de>
2017-04-06 19:25:26 +0200

    chnage javadox generation

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -11,9 +11,8 @@ git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/deichbrise/binf gh-pages > /dev/null
 
 # Commit and Push the Changes
-ls -al ./
 cd gh-pages
-git rm -rf ./*
+rm -rf ./*
 cp -Rf ../javadoc/* .
 git add -f .
 git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
