3f8650bffebe278999c13d4b0ab09406856c5968
Julia <jfriesen@uos.de>
2017-05-15 03:07:44 +0200

    Aufgabe 3

---
+++ src/com/week4/library/LibraryItem.java
@@ -0,0 +1,33 @@
+package com.week4.library;
+
+/**
+ * Created by Julia on 15.05.2017.
+ */
+public abstract class LibraryItem {
+
+    private boolean isBorrowed = false;
+
+    public LibraryItem(){}
+
+    /**
+     * Gives information about the (un)availability of the item.
+     * @return true if item is borrowed and unavailable
+     *         false if item is not yet borrowed
+     */
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    /**
+     * Marks item borrowed or available.
+     * @param isBorrowed
+     */
+    public void setBorrowed(boolean isBorrowed) {
+        this.isBorrowed = isBorrowed;
+    }
+
+    /**
+     * Is implemented by Book and BluRay
+     */
+    public abstract String getDescription();
+}
