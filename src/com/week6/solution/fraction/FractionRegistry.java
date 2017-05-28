b9bbbf49bf2c25656b8279f20350ef809e5d1511
Pascal Stammer <stammer@deichbrise.de>
2017-05-28 09:59:00 +0200

    tests and olutions

---
+++ src/com/week6/solution/fraction/FractionRegistry.java
@@ -0,0 +1,25 @@
+package com.week6.solution.fraction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author pascalstammer
+ * @version 27.05.17.
+ */
+public class FractionRegistry {
+
+    private Map<Double, Fraction> registry = new HashMap<>();
+    public boolean has(final Double key) {
+        return registry.containsKey( key );
+    }
+
+    public void add(final Double key, final Fraction fraction) {
+        registry.put( key, fraction );
+    }
+
+    public Fraction get(final Double key) {
+        return registry.get( key );
+    }
+
+}
---
+++ src/com/week6/test/fraction/FractionTest.java
@@ -0,0 +1,65 @@
+package com.week6.test.fraction;
+
+import com.common.test.AbstractTest;
+import com.common.test.Test;
+import com.week6.solution.fraction.Fraction;
+
+import static com.common.test.Assert.*;
+
+/**
+ * @author pascalstammer
+ * @version 27.05.17.
+ */
+@Test
+public class FractionTest extends AbstractTest {
+
+    public static void main( String[] args ) {
+        final FractionTest instance = new FractionTest();
+
+        instance.runAllTests();
+    }
+
+    @Test
+    public void testConstruction() {
+        final Fraction fraction1 = Fraction.ofValue( 1 );
+        final Fraction fraction2 = Fraction.ofValue( 1 );
+        final Fraction fraction3 = Fraction.ofValue( 2 );
+
+        assertTrue( fraction1 == fraction2 );
+        assertFalse( fraction1 == fraction3 );
+    }
+
+    @Test
+    public void testSameReferenceIfInstantiatedWithParseFraction() {
+        final Fraction fraction1 = Fraction.parseFraction( "1/2" );
+        final Fraction fraction2 = Fraction.parseFraction( "2/4" );
+        final Fraction fraction3 = Fraction.parseFraction( "-1/2" );
+
+        assertTrue( fraction1 == fraction2 );
+        assertFalse( fraction1 == fraction3 );
+    }
+
+    @Test
+    public void testSameReferenceIfMultiply() {
+        final Fraction fraction1 = Fraction.ofValue( 1, 2 );
+        final Fraction fraction2 = Fraction.ofValue( 1, 2 );
+        final Fraction actualResult = fraction1.multiply( fraction2 );
+        final Fraction expectedResult = Fraction.ofValue( 1, 4 );
+
+        assertTrue( expectedResult == actualResult );
+        assertEquals( 1, actualResult.getNumerator() );
+        assertEquals( 4, actualResult.getDenominator() );
+    }
+
+    @Test
+    public void testSameReferenceIfAdded() {
+        final Fraction fraction1 = Fraction.ofValue( 1, 4 );
+        final Fraction fraction2 = Fraction.ofValue( 1, 4 );
+        final Fraction actualResult = fraction1.add( fraction2 );
+        final Fraction expectedResult = Fraction.ofValue( 1, 2 );
+
+        assertTrue( expectedResult == actualResult );
+        assertEquals( 1, actualResult.getNumerator() );
+        assertEquals( 2, actualResult.getDenominator() );
+    }
+}
