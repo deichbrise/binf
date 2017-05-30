3d7c49a5e54ad758c85679838d5e2328d398e3b1
Pascal Stammer <stammer@deichbrise.de>
2017-05-30 09:39:13 +0200

    solution task 1

--- src/com/week6/solution/exception/UncertainException.java
+++ src/com/week6/solution/exception/UncertainException.java
@@ -62,11 +62,11 @@ public class UncertainException {
       try {
          throw new RuntimeException();
       } catch (RuntimeException e) {
-         i++;
+         i++; // wird ausgeführt
          throw new ClassCastException();
       } finally {
-         i++;
-         throw new NumberFormatException();
+         i++; // wird ausgeführt
+         throw new NumberFormatException(); // wird geworfen und überdeckt ClassCastException
       }
    }
 
@@ -75,10 +75,10 @@ public class UncertainException {
          try {
             break;
          } catch (RuntimeException e) {
-            i++;
+            i++; // wird nicht ausgeführt, da im try block keine exception geworfen wird
          } finally {
-            i++;
-            throw new RuntimeException();
+            i++; // wird ausgeführt, da finally immer ausgeführt wirde
+            throw new RuntimeException(); // wird geworfen
          }
       }
       //return i++;
@@ -90,70 +90,70 @@ public class UncertainException {
          try {
             throw new RuntimeException();
          } catch (RuntimeException e) {
-            i++;
+            i++; // wird ausgeführt, da im try block exception geworfen wurde
             continue;
          } finally {
-            i++;
+            i++; // wird ausgeführt, da finally immer ausgeführt wirde
          }
-      } while (false);
-      return i++;
+      } while (false); // ein schleifendurchlauf
+      return i++; // wird erst zurückgegeben, dann um 1 erhöht
    }
 
    public int uncertain4() {
       try {
-         return i++;
+         return i++; // wird erst zurückgegeben, dann um 1 erhöht
       } catch (RuntimeException e) {
          i++;
       } finally {
-         i++;
+         i++; // wird ausgeführt, da finally immer ausgeführt wird
       }
       return i++;
    }
 
    public int uncertain5() {
       try {
-         return i;
+         return i; // wird 0 zurückgegeben
       } finally {
-         throw new RuntimeException();
+         throw new RuntimeException(); // finally wird immer ausgeführt
       }
    }
 
    public int uncertain6() {
       try {
-         throw new RuntimeException();
+         throw new RuntimeException(); // exception wird einfach durch finally ignoriert
       } finally {
-         return ++i;
+         return ++i; // wird um 1 erhöht und zurückgegeben
       }
    }
 
    public int uncertain7() {
       try {
-         throw new java.io.IOException();
+         throw new java.io.IOException(); // wird nicht durch RuntimeException abgefangen
       } catch (RuntimeException e) {
-         i++;
+         i++; // wird nicht ausgeführt
       } finally {
-         return i++;
+         return i++; // wird ausgeführt
       }
    }
 
    public int uncertain8() {
       try {
-         throw new NumberFormatException();
+         throw new NumberFormatException(); // wird im catch block behandelt, da Subklasse von RuntimeException
       } catch (RuntimeException e) {
-         i++;
-         throw new RuntimeException();
+         i++; // wird um 1 erhöht
+         throw new RuntimeException(); // wird geworfen
       } finally {
-         i++;
+         i++; // wird im 1 erhöht
       }
    }
 
    public int uncertain9() {
       try {
-         throw new ClassCastException();
+         throw new ClassCastException(); // wird abgefangen, da RuntimeException
       } catch (RuntimeException e) {
-         return i++;
+         return i++; // wird um 1 erhöht
       } finally {
-         return i++;
+         return i++; // wird erst zurückgegeben, dann um 1 erhöht
       }
    }
 
@@ -161,10 +161,10 @@ public class UncertainException {
       try {
           //java: unreported exception java.io.IOException; must be caught or declared to be thrown
           //Korrektur: Methodenkopf um throws IOException erweitert
-         throw new java.io.IOException();
+         throw new java.io.IOException(); // wird nicht behandelt, deswegen wird die exception geworfen und kann von außen abgefangen werden
       } catch (RuntimeException e) {
       }
-      return 1;
+      return 1; // wird nicht ausgeführt
 
    }
 }
