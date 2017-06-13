8331ba611f3283fc40d51e93aadf7fb54dde0172
Pascal Stammer <stammer@deichbrise.de>
2017-06-13 07:10:17 +0200

    clean

--- src/com/week7/solution/io/LineReader.java
+++ src/com/week7/solution/io/LineReader.java
@@ -1,12 +1,8 @@
 package com.week7.solution.io;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.Reader;
-import java.nio.CharBuffer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,20 +18,21 @@ public class LineReader extends LineNumberReader {
     private Integer lineNumber;
 
     public LineReader( final Reader reader, String regex ) {
-        super(reader);
+        super( reader );
         this.pattern = Pattern.compile( regex );
         this.lineNumber = -1;
     }
 
     /**
-     * Reads one line via BufferedReader
+     * Reads one line
      * Increases count of read lines
+     *
      * @return String last line which was read
      * @throws IOException if an I/O error occurs
      */
     public String readLine() throws IOException {
         lastLine = super.readLine();
-        if(lineNumber == -1) {
+        if ( lineNumber == -1 ) {
             lineNumber = 1;
         } else {
             lineNumber++;
@@ -46,7 +43,7 @@ public class LineReader extends LineNumberReader {
     public Integer getAmountOfMatches() {
         final Matcher matcher = pattern.matcher( lastLine );
         int count = 0;
-        while(matcher.find()) {
+        while ( matcher.find() ) {
             count++;
         }
         return count;
