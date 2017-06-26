5eb015060ca4743ed7a9576d2e79ede486deecd9
Pascal Stammer <stammer@deichbrise.de>
2017-06-26 13:41:02 +0200

    file utils

--- src/com/week9/solution/thread/util/FileUtil.java
+++ src/com/week9/solution/thread/util/FileUtil.java
@@ -1,7 +1,6 @@
 package com.week9.solution.thread.util;
 
 import com.week9.solution.thread.collector.FileCollector;
-import com.week9.solution.thread.collector.FileFilter;
 import com.week9.solution.thread.collector.impl.FileCollectorImpl;
 
 import java.io.File;
@@ -15,46 +14,25 @@ import java.util.List;
 public class FileUtil {
 
     private static final int NOT_FOUND = -1;
-
     public static final char EXTENSION_SEPARATOR = '.';
-
-    public static final String EXTENSION_SEPARATOR_STR = Character.toString( EXTENSION_SEPARATOR );
-
     private static final char UNIX_SEPARATOR = '/';
-
     private static final char WINDOWS_SEPARATOR = '\\';
-
-    private static final char SYSTEM_SEPARATOR = File.separatorChar;
-
     public static final long ONE_KB = 1024;
     public static final BigInteger ONE_KB_BI = BigInteger.valueOf( ONE_KB );
-    public static final long ONE_MB = ONE_KB * ONE_KB;
     public static final BigInteger ONE_MB_BI = ONE_KB_BI.multiply( ONE_KB_BI );
-    private static final long FILE_COPY_BUFFER_SIZE = ONE_MB * 30;
-    public static final long ONE_GB = ONE_KB * ONE_MB;
     public static final BigInteger ONE_GB_BI = ONE_KB_BI.multiply( ONE_MB_BI );
-    public static final long ONE_TB = ONE_KB * ONE_GB;
     public static final BigInteger ONE_TB_BI = ONE_KB_BI.multiply( ONE_GB_BI );
-    public static final long ONE_PB = ONE_KB * ONE_TB;
     public static final BigInteger ONE_PB_BI = ONE_KB_BI.multiply( ONE_TB_BI );
-    public static final long ONE_EB = ONE_KB * ONE_PB;
     public static final BigInteger ONE_EB_BI = ONE_KB_BI.multiply( ONE_PB_BI );
-    public static final BigInteger ONE_ZB = BigInteger.valueOf( ONE_KB ).multiply( BigInteger.valueOf( ONE_EB ) );
-    public static final BigInteger ONE_YB = ONE_KB_BI.multiply( ONE_ZB );
 
-    public static List<File> listFiles( final File file ) {
-        return listFiles( file, null );
+    public static List<File> listFilesRecursively( final File file ) {
+        return listFilesRecursively( file, null );
     }
 
-    public static List<File> listFiles( final File file, final String extension ) {
+    public static List<File> listFilesRecursively( final File file, final String extension ) {
         final FileCollector fileCollector = new FileCollectorImpl();
         if ( extension != null ) {
-            fileCollector.addFilter( new FileFilter() {
-                @Override
-                public boolean check( final File file ) {
-                    return file.isFile() && getExtension( file.getAbsolutePath() ).equals( extension );
-                }
-            } );
+            fileCollector.addFilter( file1 -> file1.isFile() && getExtension( file1.getAbsolutePath() ).equals( extension ) );
         }
 
         return fileCollector.collect( file );
@@ -91,7 +69,7 @@ public class FileUtil {
     }
 
     public static long getSize( File file ) {
-        final List<File> files = listFiles( file );
+        final List<File> files = listFilesRecursively( file );
 
         long size = 0;
 
